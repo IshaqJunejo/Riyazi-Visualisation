44877f02038fd5c20694d58348ff14e562440a26
Muhammad Ishaque Junejo <ishaquejunejo33@gmail.com>
2024-01-03 17:08:43 +0500

    Linear Equation is being Visualized correctly!

---
+++ Scripts/Linear-eq.js
@@ -0,0 +1,80 @@
+let yLeft = 0;
+let yRight = 0;
+
+let factor = canvas_1.width / 30;
+let m;
+let n;
+
+updateValues();
+
+function updateValues()
+{
+    ctx_1.clearRect(0, 0, canvas_1.width, canvas_1.height);
+    m = parseInt(slider_1.value);
+    n = parseInt(slider_2.value);
+    yLeft = (160 * m) + n * factor;
+    yRight = (-160 * m) + n * factor;
+    drawCoordinates();
+    drawLine();
+
+    console.log(yLeft);
+    console.log(yRight);
+}
+
+function drawCoordinates()
+{
+    // Rectangle Around
+    ctx_1.strokeRect(0, 0, canvas_1.width, canvas_1.height);
+    
+    // X-Axis
+    ctx_1.beginPath();
+    ctx_1.moveTo(canvas_1.width / 2, 0);
+    ctx_1.lineTo(canvas_1.width / 2, canvas_1.height);
+    ctx_1.stroke();
+
+    // Y-Axis
+    ctx_1.beginPath();
+    ctx_1.moveTo(0, canvas_1.height / 2);
+    ctx_1.lineTo(canvas_1.width, canvas_1.height / 2);
+    ctx_1.stroke();
+
+    for (let i = 0; i <= 30; i++)
+    {
+        ctx_1.beginPath();
+        ctx_1.arc(canvas_1.width / 2, i * factor, 2, 0, 2 * Math.PI);
+        ctx_1.stroke();
+
+        ctx_1.beginPath();
+        ctx_1.arc(i * factor, canvas_1.height / 2, 2, 0, 2 * Math.PI);
+        ctx_1.stroke();
+    }
+}
+
+function drawFunction()
+{
+    ctx_1.beginPath();
+    ctx_1.moveTo(xAtBottom + canvas_1.width / 2, -15 + canvas_1.height / 2);
+    ctx_1.lineTo(15 + canvas_1.width / 2, yAtRight + canvas_1.height / 2);
+    ctx_1.stroke();
+
+    ctx_1.beginPath();
+    ctx_1.arc(xAtBottom + canvas_1.width / 2, (-15 + canvas_1.height / 2) * -1, 5, 0, Math.PI);
+    ctx_1.stroke();
+
+    ctx_1.beginPath();
+    ctx_1.arc(15 + canvas_1.width / 2, (yAtRight + canvas_1.height / 2) * -1, 5, Math.PI, 2 * Math.PI);
+    ctx_1.stroke();
+
+    ctx_1.beginPath();
+    ctx_1.moveTo(0, canvas_1.height);
+    ctx_1.lineTo(canvas_1.width, 0);
+    ctx_1.stroke();
+}
+
+function drawLine()
+{
+    ctx_1.beginPath();
+    ctx_1.moveTo(0, -yRight + canvas_1.height / 2);
+    ctx_1.lineTo(canvas_1.width, -yLeft + canvas_1.height / 2);
+    ctx_1.stroke();
+}
